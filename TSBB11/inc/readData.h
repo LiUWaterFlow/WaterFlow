bd0f026726fd12964088f5cfdcb6df815305b76d
Conrad Wahlén <conwa099@student.liu.se>
2015-09-14 19:35:02 +0200

    Extended the read class to also generate terrain

--- TSBB11/inc/readData.h
+++ TSBB11/inc/readData.h
@@ -1,15 +1,33 @@
+#include "VectorUtils3.h"
+#include "loadobj.h"
+
 #include <vector>
 
 struct mapdata {
 	int	ncols;
 	int nrows;
-	double xllcorner;
-	double yllcorner;
-	double cellsize;
-	double NODATA_value;
-	std::vector<double> data; 
-	
-	double getData(int col, int row);
+	float xllcorner;
+	float yllcorner;
+	float cellsize;
+	float NODATA_value;
+	std::vector<float> data; 
 };
 
-mapdata* readDEM(const char* inputfile);
+
+class DataHandler 
+{
+	mapdata* readdata;
+	
+	void readDEM(const char* inputfile);
+	Model* GenerateTerrain(GLfloat tScale);
+
+public:
+	Model* datamodel;
+
+	DataHandler(const char* inputfile, GLfloat tScale = 5000.0f);
+	~DataHandler();
+	float getData(int col, int row);
+};
+
+vec3 giveNormal(int x, int y, int z, GLfloat *vertexArray, GLuint *indexArray, int width, int height);
+GLfloat giveHeight(GLfloat x, GLfloat z, GLfloat *vertexArray, int width, int height);
