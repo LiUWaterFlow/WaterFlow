8adc6a8cb81981eb1a738ceccd3e117d2dae35b5
godski <godymirski@gmail.com>
2015-10-06 00:36:18 +0200

    Adding new files

---
+++ TSBB11/src/xml/testing.cpp
@@ -0,0 +1,38 @@
+#include "xmlParsing.h"
+
+int main()
+{
+  float tw, r;
+  std::vector<int> pos1, pos2;
+  std::vector<float> ndirec;
+  std::vector<FlowSource*> sfv = loadFlows("xgconsole.xml");
+  FlowSource first = *sfv[0];
+  for(int i = 0; i < 12; i++){
+    std::cout << "Pressure: " << first.getPressure() << std::endl;
+    ndirec = first.getNormal();
+    std::cout << "Normal given by xyz: ";
+    for (auto j = ndirec.begin(); j != ndirec.end(); ++j){
+      std::cout << *j << ' ';
+    }
+    std::cout << std::endl;
+    first.update();
+  }
+
+  pos1 = sfv[0]->getPosition();
+  pos2 = sfv[1]->getPosition();
+  tw = sfv[0]->getWaterLeft();
+  r = sfv[0]->getRadius();
+  std::cout << "Postion given by xyz: ";
+  for (auto i = pos1.begin(); i != pos1.end(); ++i){
+    std::cout << *i << ' ';
+  }
+  std::cout << std::endl;
+  std::cout << "Postion given by xyz: ";
+  for (auto i = pos2.begin(); i != pos2.end(); ++i){
+    std::cout << *i << ' ';
+  }
+  std::cout << std::endl;
+  std::cout << "TotWater: " << tw << std::endl;
+  std::cout << "Radius: " << r << std::endl;
+  return 1;
+}
---
+++ TSBB11/src/xml/xmlParsing.h
@@ -0,0 +1,26 @@
+#ifndef XMLPARSING_H
+#define XMLPARSING_H
+#include "pugixml.h"
+#include "flowSource.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+std::vector<float> fStrToVector(std::string str);
+
+std::vector<int> iStrToVector(std::string str);
+
+void parsePressure(FlowSource* obj, pugi::xml_node pres, pugi::xml_node time);
+
+void parsePosition(FlowSource* obj, pugi::xml_node node);
+
+void parseNormal(FlowSource* obj, pugi::xml_node norm, pugi::xml_node time);
+
+void parseTotalWater(FlowSource* obj, pugi::xml_node node);
+
+void parseRadius(FlowSource* obj, pugi::xml_node node);
+
+std::vector<FlowSource*> loadFlows(const char* xmlFile);
+
+#endif
