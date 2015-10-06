7439f726001b1225cfa200bea5abfb7a48463662
Conrad <conrad.wahlen@gmail.com>
2015-10-06 15:22:53 +0000

    Auto-updating Doxygen developer documentation

--- html/search/variables_73.js
+++ html/search/variables_73.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['samplefactor',['sampleFactor',['../class_data_handler.html#ad13bf156e22a6c65985e779cd40f3411',1,'DataHandler']]],
   ['squaremodel',['squareModel',['../class_data_handler.html#a47df3b25b223addf0dd209f0e5044727',1,'DataHandler']]]
 ];
