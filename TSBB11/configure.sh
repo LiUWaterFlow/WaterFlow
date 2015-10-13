413a0ea6018ddd2403bb48bd48e3cfe2ce9f149b
Jens Edhammer <tobyndax@hotmail.com>
2015-10-13 17:14:26 +0200

    Restructured AntTweak libraries.

--- TSBB11/configure.sh
+++ TSBB11/configure.sh
@@ -5,11 +5,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         echo "OSX system detected - Setting up"
 
         echo "Copying AntTweak to /usr/local/lib/ (non overwriting)"
-        cp -n lib/AntTweak/lib/libAntTweakbar.a /usr/local/lib/libAntTweakbar.a
-        cp -n lib/AntTweak/lib/libAntTweakbar.dylib /usr/local/lib/libAntTweakbar.dylib
+        cp -n lib/AntTweak/libAntTweakbar.a /usr/local/lib/libAntTweakbar.a
+        cp -n lib/AntTweak/libAntTweakbar.dylib /usr/local/lib/libAntTweakbar.dylib
 
-        echo "Copying the SDL frameworks"
-        cp -nR lib/SDL2.framework /Library/Frameworks/SDL2.framework
+        echo "Copying the SDL frameworks, requires sudo"
+        sudo cp -R lib/SDL2.framework /Library/Frameworks/
 else
 echo "Non OSX system detected, this script does not support your system. Please refer to the wiki on https://github.com/LiUWaterFlow/WaterFlow/wiki for installation instructions for your system."
 fi
