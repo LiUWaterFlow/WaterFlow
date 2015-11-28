accfb6634117d082bb0ecb338d6ec3276b31b34a
Jens Edhammer <tobyndax@hotmail.com>
2015-11-28 15:12:50 +0100

    Mouse contained in window again, and volume measurement works (except for it lowering performance after it is done).

--- TSBB11/src/heightField.cpp
+++ TSBB11/src/heightField.cpp
@@ -309,16 +309,16 @@ void HeightField::measureVolume(){
 	float* u = new float[texWidth*texHeight];
 
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, fieldBuffers[0]);
-	glGetSubData(GL_SHADER_STORAGE_BUFFER, GL_READ_ONLY );
-
+	glGetBufferSubData(GL_SHADER_STORAGE_BUFFER,0,texWidth*texHeight*sizeof(float),u);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+	
 	std::valarray<float> myvalarray(u, texWidth*texHeight);
 	float vol1 = myvalarray.sum();
-	glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 	
 
 	std::cout << "Volume difference is: " << vol1- vol0 - addedVol << std::endl;
 	myvalarray.resize(0); 
+	delete u;
 	
 }
 
